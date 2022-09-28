df090cac0c225fac34adbd619163947e7ff63fa5
CDA <cathyda97@gmail.com>
2022-09-28 02:19:38 -0400

    Header, Footer, and Logo added

--- app/routes/index.route.server.js
+++ app/routes/index.route.server.js
@@ -1,6 +1,6 @@
 //index.route.server.js Cathy Da 301177731 Sept 26
-import {Router} from "express";
-import { displayAboutPage, displayContactPage, displayHomePage, displayProjectsPage, displayServicesPage } from "../controllers/index.controller.server";
+import { Router } from "express";
+import { displayAboutPage, displayContactPage, displayHomePage, displayProjectsPage, displayServicesPage } from "../controllers/index.controller.server.js";
 
 const router = Router();
 
--- server.js
+++ server.js
@@ -4,6 +4,7 @@
 import express from 'express';
 import logger from 'morgan';
 import session from 'express-session';
+import cookieParser from 'cookie-parser';
 
 //__dirname fix
 import path, {dirname} from 'path';
@@ -11,13 +12,22 @@ import {fileURLToPath} from 'url';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 //import routes
-import indexRouter from './app/routes/index.route.server.js'
-
-//config module
+import indexRouter from './app/routes/index.route.server.js';
 
 //instantiate express application
 const app = express();
 
+//set up viewengine ejs
+app.set('views', path.join(__dirname, '/app/views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '/public')));
+
+//use routes
 app.use('/', indexRouter)
 
-app.listen(3000);
+app.listen(3000)
